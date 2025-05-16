cbc17824c3d0acdf4a440005f723f988c3ac2f70
BekerDi <143925720+BekerDi@users.noreply.github.com>
2025-05-16 13:18:47 +0000

    dzDiana1

--- homeWork/05/template1/main.cpp
+++ homeWork/05/template1/main.cpp
@@ -4,5 +4,7 @@
 using namespace std;
 
 int main(){
+    std::cout << "Fibonacci<4>::value = " << Fibonacci<4>::value << std::endl;
+    std::cout << "fibonacci<4>() = " << fibonacci<4>() << std::endl;
     return 0;
 }
--- homeWork/05/template1/template.hpp
+++ homeWork/05/template1/template.hpp
@@ -1,13 +1,39 @@
-#pragma once 
+#pragma once
 #include <cmath>
 #define EPS 1e-10
 
 
-auto fibonacci() -> T{
+template<int N>
+auto fibonacci() -> int {
+    return fibonacci<N - 1>() + fibonacci<N - 2>();
+}
 
+template<>
+auto fibonacci<0>() -> int {
+    return 0;
+}
+
+template<>
+auto fibonacci<1>() -> int {
+    return 1;
 }
 
 
-class Fibonacci{
+template<int N>
+class Fibonacci {
+public:
+    static const int value = Fibonacci<N - 1>::value + Fibonacci<N - 2>::value;
+};
 
+
+template<>
+class Fibonacci<0> {
+public:
+    static const int value = 0;
+};
+
+template<>
+class Fibonacci<1> {
+public:
+    static const int value = 1;
 };
